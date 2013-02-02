6320590fb4cd164aeb397153cd4fc86ecce57fbc
Nils Werner <wernerns@iis.fraunhofer.de>
2013-02-02 18:38:51 +0100

    Copied stuff over from LESS compiler

---
+++ extension.driver.php
@@ -0,0 +1,88 @@
+<?php
+
+	Class extension_LESS_Compiler extends Extension{
+
+		public function about(){
+			return array(
+				'name' => 'LESS Compiler',
+				'version' => '1.0',
+				'release-date' => '2011-09-04',
+				'author' => array(
+					array(
+						'name' => 'Nils Werner',
+						'website' => 'http://www.obssessive-media.de/',
+						'email' => 'nils.werner@gmail.com'
+					)
+				)
+			);
+		}
+
+		public function getSubscribedDelegates(){
+			return array();
+		}
+
+		public function install(){
+			General::realiseDirectory(CACHE . '/less_compiler/', Symphony::Configuration()->get('write_mode', 'directory'));
+			
+			$htaccess = @file_get_contents(DOCROOT . '/.htaccess');
+
+			if($htaccess === false) return false;
+
+			## Cannot use $1 in a preg_replace replacement string, so using a token instead
+			$token = md5(time());
+
+			$rule = "
+	### LESS RULES
+	RewriteRule ^less\/(.+\.less)\$ extensions/less_compiler/lib/less.php?param={$token} [L,NC]\n\n";
+
+			## Remove existing the rules
+			$htaccess = self::__removeLessRules($htaccess);
+
+			if(preg_match('/### LESS RULES/', $htaccess)){
+				$htaccess = preg_replace('/### LESS RULES/', $rule, $htaccess);
+			}
+			else{
+				$htaccess = preg_replace('/RewriteRule .\* - \[S=14\]\s*/i', "RewriteRule .* - [S=14]\n{$rule}\t", $htaccess);
+			}
+
+			## Replace the token with the real value
+			$htaccess = str_replace($token, '$1', $htaccess);
+
+			return @file_put_contents(DOCROOT . '/.htaccess', $htaccess);
+		}
+
+		public function uninstall(){
+			$htaccess = @file_get_contents(DOCROOT . '/.htaccess');
+
+			if($htaccess === false) return false;
+
+			$htaccess = self::__removeLessRules($htaccess);
+			$htaccess = preg_replace('/### LESS RULES/', NULL, $htaccess);
+
+			return @file_put_contents(DOCROOT . '/.htaccess', $htaccess);
+		}
+
+		public function enable(){
+			return $this->install();
+		}
+
+		public function disable(){
+			$htaccess = @file_get_contents(DOCROOT . '/.htaccess');
+
+			if($htaccess === false) return false;
+
+			$htaccess = self::__removeLessRules($htaccess);
+			$htaccess = preg_replace('/### LESS RULES/', NULL, $htaccess);
+
+			return @file_put_contents(DOCROOT . '/.htaccess', $htaccess);
+		}
+
+	/*-------------------------------------------------------------------------
+		Utilities:
+	-------------------------------------------------------------------------*/
+
+		private static function __removeLessRules($string){
+			return preg_replace('/RewriteRule \^less[^\r\n]+[\r\n]?/i', NULL, $string);
+		}
+
+	}
---
+++ lib/less.php
@@ -0,0 +1,40 @@
+<?php
+
+	@ini_set('display_errors', 'off');
+
+	define('DOCROOT', rtrim(realpath(dirname(__FILE__) . '/../../../'), '/'));
+	define('DOMAIN', rtrim(rtrim($_SERVER['HTTP_HOST'], '/') . str_replace('/extensions/less_compiler/lib', NULL, dirname($_SERVER['PHP_SELF'])), '/'));
+
+	// Include some parts of the engine
+	require_once(DOCROOT . '/symphony/lib/boot/bundle.php');
+	require_once(CONFIG);
+	
+	require_once('dist/lessc.inc.php');
+	
+	function processParams($string){
+		$param = (object)array(
+			'file' => 0
+		);
+
+		if(preg_match_all('/^(.+)$/i', $string, $matches, PREG_SET_ORDER)){
+			$param->file = $matches[0][1];
+		}
+		
+		return $param;
+	}
+	
+	$param = processParams($_GET['param']);
+	
+	header('Content-type: text/css');
+	
+	$lc = new lessc(WORKSPACE . '/' . $param->file);
+	$css = $lc->parse();
+	
+	$filename = pathinfo($param->file);
+	$filename = $filename['filename'];
+	
+	file_put_contents(CACHE . '/less_compiler/' . $filename . '.css', $css);
+	
+	echo $css;
+	
+	exit;
