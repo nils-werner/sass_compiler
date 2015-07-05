a7f56ee786bc99c88986744d8176ef90cde091d9
Tintin81 <tik@dsc.qmul.ac.uk>
2015-07-05 11:14:01 +0200

    Update for Symphony 2.6

--- lib/sass.php
+++ lib/sass.php
@@ -6,7 +6,7 @@
 	define('DOMAIN', rtrim(rtrim($_SERVER['HTTP_HOST'], '/') . str_replace('/extensions/sass_compiler/lib', NULL, dirname($_SERVER['PHP_SELF'])), '/'));
 
 	// Include some parts of the engine
-	require_once(DOCROOT . '/symphony/lib/boot/bundle.php');
+	require_once(DOCROOT . '/vendor/autoload.php');
 	require_once(CONFIG);
 	
 	require_once('dist/SassParser.php');
